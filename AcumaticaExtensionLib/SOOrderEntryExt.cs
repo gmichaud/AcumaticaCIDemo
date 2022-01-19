3672d3b09477e2cfba5026f9e286175969817365
Gabriel Michaud <gabriel@velixo.com>
2022-01-19 08:29:59 -0500

    Add extension library

---
+++ AcumaticaExtensionLib/SOOrderEntryExt.cs
@@ -0,0 +1,20 @@
+﻿using PX.Data;
+using PX.Objects.SO;
+using System;
+
+namespace AcumaticaExtensionLib
+{
+    public class SOOrderEntryExt : PXGraphExtension<SOOrderEntry>
+    {
+        [PXOverride]
+        public void Persist(Action baseMethod)
+        {
+            if (Base.Document.Current.CuryOrderTotal >= 1000 && String.IsNullOrWhiteSpace(Base.Document.Current.CustomerOrderNbr))
+            { 
+                throw new PXException("Customer order number is required for orders of more than $1,000");
+            }
+
+            baseMethod();
+        }
+    }
+}
